c4839ad9c0f408074c61738f2e676eafb87edb71
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-03-17 11:08:16 +0900

    5

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,21 +1,41 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function Forms() {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log("Im valid bby");
+  };
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: "username is required",
+          minLength: {
+            message: "the username should be longer than chars",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="email" required />
       <input
-        {...register("password")}
+        {...register("email", { required: "Email is required" })}
+        type="email"
+        placeholder="email"
+      />
+      <input
+        {...register("password", { required: "password is required" })}
         type="password"
         placeholder="password"
-        required
       />
       <input type="submit" placeholder="Create Account" />
     </form>
