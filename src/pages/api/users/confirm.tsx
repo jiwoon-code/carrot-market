57bf3915096f0ccd33dda8b560f52da78f58634a
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-03-22 16:38:18 +0900

    session

--- src/pages/api/users/confirm.tsx
+++ src/pages/api/users/confirm.tsx
@@ -1,14 +1,28 @@
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import smtpTransport from "@libs/server/email";
+import { withIronSessionApiRoute } from "iron-session/next";
+import client from "@libs/server/client";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "4905943850934850985934sklfjlksjdfjsl;fsjeflishlfksjefo;hsefjoief",
+});
