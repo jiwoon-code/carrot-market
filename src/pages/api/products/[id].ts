15e232482af2b95dfb4540719c58a5a24d5c391f
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-03-24 13:14:35 +0900

    product detail

---
+++ src/pages/api/products/[id].ts
@@ -0,0 +1,34 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query; //해당하는 product찾기
+  const product = await client.product.findUnique({
+    where: {
+      id: +id!.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  console.log(product);
+  res.json({ ok: true, product });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
