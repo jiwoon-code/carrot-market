ed5525390fb62a6ee011ef61a1b9253bf4cf96b4
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-03-29 14:19:23 +0900

    all posts

--- src/pages/community/[id].tsx
+++ src/pages/community/[id].tsx
@@ -78,6 +78,7 @@ const CommunityPostDetail: NextPage = () => {
   useEffect(() => {
     if (answerData && answerData.ok) {
       reset();
+      mutate();
     }
   }, [answerData, reset]);
   return (
@@ -146,7 +147,7 @@ const CommunityPostDetail: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              <span>답변 {data?.post._count.answers}</span>
+              <span>답변 {data?.post?._count.answers}</span>
             </span>
           </div>
         </div>
